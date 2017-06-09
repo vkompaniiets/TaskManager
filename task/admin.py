02c18876e68cd26ab2a726c385aaeecebd25fe9f
vkompaniiets <haden921@gmail.com>
2017-06-09 22:17:55 +0300

    Add task model to admin panel

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from task.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('assignee', 'created_by', 'name', 'completed')
+    raw_id_fields = ('assignee', )
+    list_per_page = 50
+
+admin.site.register(Task, TaskAdmin)
