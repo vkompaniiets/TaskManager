301763652b4e664b1f40c3ef052a0619073dfe0f
vkompaniiets <haden921@gmail.com>
2017-06-09 21:30:28 +0300

    Fix login route

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 from account import views
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/account/login/')),
     url(r'^login/', views.account_login, name='login'),
     url(r'^logout/', views.account_logout, name='logout'),
     url(r'^register/', views.account_register, name='register'),
