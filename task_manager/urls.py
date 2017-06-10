e416e8e6f8a624421d23b5c1a4dfce3ac6fd35a4
vkompaniiets <haden921@gmail.com>
2017-06-10 11:54:59 +0300

    Update route for admin static

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.views.static import serve
 from django.views.generic import RedirectView
 
 urlpatterns = [
@@ -7,4 +9,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('account.urls')),
     url(r'^tasks/', include('task.urls')),
+    url(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    })
 ]
